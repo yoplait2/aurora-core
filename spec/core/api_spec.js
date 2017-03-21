73ec4888c8ff963ed310046250b6c6c6f6d4af2c
Federico Minzoni <fminzoni@enter.it>
2017-03-21 17:27:07 +0100

    fix timeout

--- spec/core/api_spec.js
+++ spec/core/api_spec.js
@@ -5,14 +5,14 @@ var app = require("../../build/app.js")
 
 var base_url = "http://localhost:3000/core/identity"
 
-beforeAll(function (done) {
-    setTimeout(function () {
-        done()
-    }, 5000);
-});
-
 describe("gateway", function() {
   describe("GET default route /core/identity", function() {
+    beforeAll(function (done) {
+      setTimeout(function () {
+          done()
+      }, 5000);
+    });
+
     it("returns status code 200", function(done) {
       request.get(base_url, function(error, response, body) {
         expect(response.statusCode).toBe(200);
