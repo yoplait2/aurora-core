d53adad0f484c73ee957c807a728c582b942229e
Federico Minzoni <fminzoni@enter.it>
2017-03-21 17:30:39 +0100

    refactoring test process

--- spec/core/api_spec.js
+++ spec/core/api_spec.js
@@ -1,18 +1,9 @@
 var request = require("request");
-var config = require('dotenv').config({path: '.env.ci'})
-
-var app = require("../../build/app.js")
 
 var base_url = "http://localhost:3000/core/identity"
 
 describe("gateway", function() {
   describe("GET default route /core/identity", function() {
-    beforeAll(function (done) {
-      setTimeout(function () {
-          done()
-      }, 5000);
-    });
-
     it("returns status code 200", function(done) {
       request.get(base_url, function(error, response, body) {
         expect(response.statusCode).toBe(200);
