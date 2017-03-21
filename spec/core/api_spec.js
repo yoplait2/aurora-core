ef7659d9eff687fd82c5d344ad27daf0c4128e7f
Federico Minzoni <fminzoni@enter.it>
2017-03-21 22:16:08 +0100

    add signed config

--- spec/core/api_spec.js
+++ spec/core/api_spec.js
@@ -6,13 +6,15 @@ var app = require("../../build/app.js")
 var base_url = "http://localhost:3000/api/identity"
 
 describe("gateway", function() {
+  beforeAll(function (done) {
+    setTimeout(function () {
+        console.log('start first test')
+        done()
+    }, 2000);
+  });
+
   describe("GET default route /api/identity", function() {
-    it("returns status code 200", function(done) {
-      beforeAll(function (done) {
-        setTimeout(function () {
-            done()
-        }, 2000);
-      });
+    it("returns status code 404", function(done) {
       request.get(base_url, function(error, response, body) {
         expect(response.statusCode).toBe(404);
         done();
