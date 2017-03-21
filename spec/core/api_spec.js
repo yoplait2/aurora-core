29bb8cb2a34f8aff8bd5eb7ae92c7200e1a0b094
Federico Minzoni <fminzoni@enter.it>
2017-03-21 17:17:13 +0100

    change test

--- spec/core/api_spec.js
+++ spec/core/api_spec.js
@@ -3,10 +3,10 @@ var config = require('dotenv').config({path: '.env.ci'})
 
 var app = require("../../build/app.js")
 
-var base_url = "http://localhost:3002/api"
+var base_url = "http://localhost:3000/core/identity"
 
 describe("gateway", function() {
-  describe("GET default route /api", function() {
+  describe("GET default route /core/identity", function() {
     it("returns status code 200", function(done) {
       request.get(base_url, function(error, response, body) {
         expect(response.statusCode).toBe(200);
