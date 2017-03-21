dcd29cef398124edb66853813db476e4a5d0fa6d
Federico Minzoni <fminzoni@enter.it>
2017-03-21 21:52:29 +0100

    improve test

--- spec/core/api_spec.js
+++ spec/core/api_spec.js
@@ -6,13 +6,16 @@ var app = require("../../build/app.js")
 var base_url = "http://localhost:3000/api/identity"
 
 describe("gateway", function() {
+  beforeAll(function (done) {
+    console.log('start tests');
+    setTimeout(function () {
+        console.log('start first test');
+        done()
+    }, 5000);
+  });
+
   describe("GET default route /api/identity", function() {
     it("returns status code 200", function(done) {
-      beforeAll(function (done) {
-        setTimeout(function () {
-            done()
-        }, 5000);
-      });
       request.get(base_url, function(error, response, body) {
         expect(response.statusCode).toBe(404);
         done();
