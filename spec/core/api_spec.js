110da844a0a4a6f142159b60ae7d87de13a60e0c
Federico Minzoni <fminzoni@enter.it>
2017-03-22 12:19:31 +0100

    improve unit test

--- spec/core/api_spec.js
+++ spec/core/api_spec.js
@@ -3,7 +3,7 @@ var config = require('dotenv').config({path: '.env.ci'})
 
 var app = require("../../build/app.js")
 
-var base_url = "http://localhost:3000/api/identity"
+var base_url = "http://localhost:3000/core"
 
 describe("gateway", function() {
   beforeAll(function (done) {
@@ -15,8 +15,8 @@ describe("gateway", function() {
 
   describe("GET default route /api/identity", function() {
     it("returns status code 200", function(done) {
-      request.get(base_url, function(error, response, body) {
-        expect(response.statusCode).toBe(404);
+      request.get(base_url + "/api/identity", function(error, response, body) {
+        expect(response.statusCode).toBe(200);
         done();
       });
 
